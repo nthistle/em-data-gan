f11a0784e921f34a32d5adca9c3b48fc949fdf88
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-08-14 14:32:15 -0400

    Finish basic GAN architecture

--- run_model.py
+++ run_model.py
@@ -4,22 +4,116 @@ import sys
 from PIL import Image
 from keras.layers import Reshape, Flatten, LeakyReLU, Activation
 from keras.layers.core import Dense, Dropout
-from keras.layers.convolutional import MaxPooling3D, Conv3D
+from keras.layers.convolutional import MaxPooling3D, Conv3D, Conv3DTranspose, UpSampling3D
 from keras.models import Sequential
-from keras.optimizers import Adam, SGD
+from keras_adversarial import AdversarialModel, simple_gan
+from keras_adversarial import normal_latent_sampling, AdversarialOptimizerSimultaneous
+from keras.callbacks import Callback
+import util
+from keras.optimizers import Adam
 
 
-#placeholder convolutional net
-def train_em_gan(epochs, learning_rate):
+# class debugprint(Callback):
+#
+#     def on_batch_begin(self, batch, logs=None):
+#         print("batch is beginning")
+#         print(logs)
+#
+#     def on_batch_end(self, batch, logs=None):
+#         print("batch is ending")
+#         print(logs)
+
+
+def em_generator(latent_dim, input_shape):
     model = Sequential()
-    model.add(Conv3D(32, (5,5,3), input_shape=(24,24,12,1), activation="relu"))
-    model.add(MaxPooling3D((2,2,2)))
-    model.add(Conv3D(16, (3,3,3), input_shape=(10,10,5,32), activation="relu"))
-    model.add(Dropout(0.4))
-    model.add(Dense(10, activation="relu"))
-    model.add(Dense(1))
-    model.add(Activation("sigmoid"))
-    print(model.summary())
+    #model.add(Dense(784, input_shape=(latent_dim,), activation="relu"))
+    model.add(Dense(1728, input_shape=(latent_dim,), activation="relu"))
+    model.add(Reshape([6,6,3,16]))
+    model.add(UpSampling3D((2,2,2)))
+    model.add(Conv3DTranspose(64, (5,5,3), activation="relu"))
+    model.add(Conv3DTranspose(32, (5,5,3), activation="relu"))
+    model.add(Conv3DTranspose(16, (5,5,3), activation="relu"))
+    model.add(Conv3DTranspose(16, (3,3,3), activation="relu"))
+    model.add(Conv3D(8, (3,3,3), activation="relu"))
+    model.add(Conv3D(1, (1,1,1), activation="sigmoid"))
+    return model
+
+def em_discriminator(input_shape):
+    disc = Sequential()
+    disc.add(Conv3D(128, (5,5,3), input_shape=(input_shape+(1,)), activation="relu"))
+    #disc.add(Dropout(0.2))
+    disc.add(Conv3D(64, (3,3,3), activation="relu"))
+    #disc.add(Dropout(0.2))
+    disc.add(Conv3D(32, (3,3,3), activation="relu"))
+    disc.add(Conv3D(8, (1,1,1), activation="relu"))
+    disc.add(Flatten())
+    disc.add(Dense(8))
+    disc.add(Activation("relu"))
+    disc.add(Dense(1))
+    disc.add(Activation("sigmoid"))
+    return disc
+
+def train_em_gan(adversarial_optimizer,
+                 generator, discriminator, gen_opt, disc_opt,
+                 latent_dim,
+                 h5_filename, h5_dataset_path, sample_shape,
+                 verbose=1, loss='binary_crossentropy'):
+
+    gan = simple_gan(generator, discriminator, normal_latent_sampling((latent_dim,)))
+
+    if(verbose>=1):
+        print("="*20+" Generator "+"="*20)
+        print(generator.summary())
+        print("")
+        print("="*20+" Discriminator "+"="*20)
+        print(discriminator.summary())
+        print("")
+        print("="*20+" GAN "+"="*20)
+        print(gan.summary())
+        print("")
+
+    model = AdversarialModel(base_model=gan,
+                             player_params=[generator.trainable_weights, discriminator.trainable_weights],
+                             player_names=["generator","discriminator"])
+
+    model.adversarial_compile(adversarial_optimizer=adversarial_optimizer,
+                              player_optimizers=[gen_opt, disc_opt],
+                              loss=loss)
+
+    #dbg = debugprint()
+
+    sample_generator = util.h5_block_generator(h5_filename, h5_dataset_path, sample_shape, [1,0,0,1])
+
+    model.fit_generator(sample_generator, 16, 5,
+                        verbose=verbose,
+                        validation_data=sample_generator, validation_steps=16)
+
+    # model = Sequential()
+    # model.add(Conv3D(32, (5,5,3), input_shape=(24,24,12,1), activation="relu"))
+    # model.add(MaxPooling3D((2,2,2)))
+    # model.add(Conv3D(16, (3,3,3), input_shape=(10,10,5,32), activation="relu"))
+    # model.add(Dropout(0.4))
+    # model.add(Dense(10, activation="relu"))
+    # model.add(Dense(1))
+    # model.add(Activation("sigmoid"))
+    # print(model.summary())
+
+
+def main():
+    latent_dim = 300
+    input_shape = (24, 24, 12)
+
+    generator = em_generator(latent_dim, input_shape)
+    discriminator = em_discriminator(input_shape)
+    #print(discriminator.summary())
+    #print(generator.summary())
+    train_em_gan(AdversarialOptimizerSimultaneous(),
+                 generator, discriminator,
+                 Adam(1e-4, decay=1e-4),
+                 Adam(1e-3, decay=1e-4),
+                 latent_dim,
+                 "/home/thistlethwaiten/cremi-data/sample_A_20160501.hdf","/volumes/raw", input_shape)
+
 
 if __name__=="__main__":
-    train_em_gan(1,0.001)
+    main()
--- util.py
+++ util.py
@@ -2,25 +2,29 @@ import h5py
 import numpy as np
 
 #credit for lheinric for most of this code
-def h5_block_generator(filename, path, sample_size, batch_size=16, apply=None):
+def h5_block_generator(filename, path, sample_shape, expected_output, batch_size=16):
     ds = h5py.File(filename, "r")[path]
 
     while True:
-        batch = np.empty((batch_size,) + sample_size)
+        batch = np.empty((batch_size,) + sample_shape)
 
-        z_start = np.random.random_integers(0, ds.shape[0] - sample_size[0] - 1, batch_size)
-        y_start = np.random.random_integers(0, ds.shape[1] - sample_size[1] - 1, batch_size)
-        x_start = np.random.random_integers(0, ds.shape[2] - sample_size[2] - 1, batch_size)
+        z_start = np.random.random_integers(0, ds.shape[0] - sample_shape[0] - 1, batch_size)
+        y_start = np.random.random_integers(0, ds.shape[1] - sample_shape[1] - 1, batch_size)
+        x_start = np.random.random_integers(0, ds.shape[2] - sample_shape[2] - 1, batch_size)
 
         for k in range(batch_size):
             ds.read_direct(batch,
-                           np.s_[z_start[k]:z_start[k] + sample_size[0],
-                           y_start[k]:y_start[k] + sample_size[1],
-                           x_start[k]:x_start[k] + sample_size[2]],
+                           np.s_[z_start[k]:z_start[k] + sample_shape[0],
+                           y_start[k]:y_start[k] + sample_shape[1],
+                           x_start[k]:x_start[k] + sample_shape[2]],
                            np.s_[k, :, :, :])
 
-        gt = np.zeros((batch_size, 1, 1, 1))
-
-        yield ([batch], [gt] * batch_size)
-
-
+        yield ( [np.reshape(batch, batch.shape + (1,))],
+                       [np.ones((batch_size, 1)),
+                        np.zeros((batch_size, 1)),
+                        np.zeros((batch_size, 1)),
+                        np.ones((batch_size, 1))] )
+        #print("shape is ",whattoyield[0][0].shape)
+        #print("yielding ",np.asarray(whattoyield[0]).shape)
+        #([batch], [expected_output] * batch_size)
+        #[batch]?
