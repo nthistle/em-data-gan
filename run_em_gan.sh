df47558c69df6e6acaf51a66d563ebb7fe524fb4
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-08-19 12:07:07 -0400

    Minor fix

--- run_em_gan.sh
+++ run_em_gan.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export CUDA_VISIBLE_DEVICES=$1
-python run_model.py $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 > "output/em_gan_$1_$3_$4_$5_$8_$9_$10_$11_$12.out" 2>&1
+python run_model.py $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 > "output/em_gan_$1_$2_$4_$5_$8_$9_$10_$11_$12.out" 2>&1
 
